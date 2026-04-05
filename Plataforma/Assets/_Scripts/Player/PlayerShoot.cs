8e8421f9e45fa45bd1216d17f931a06dd313f017
gizmo <niquimiqui@gmail.com>
2026-04-05 18:51:45 -0500

    Add bullet selection mechanics and new Bullet_2 prefab
    - Introduced 'ChangeBullet' action in PlayerActions.inputactions for switching between bullet types.
    - Added Bullet_2 prefab with associated components and scripts for new bullet behavior.
    - Updated PlayerShoot script to manage two bullet types and their respective ammo counts.
    - Enhanced Level-1 scene to include the new bullet type and updated references for bullet management.

--- Plataforma/Assets/_Scripts/Player/PlayerShoot.cs
+++ Plataforma/Assets/_Scripts/Player/PlayerShoot.cs
@@ -3,29 +3,61 @@ using UnityEngine.InputSystem;
 
 public class PlayerShoot : MonoBehaviour
 {
-    public GameObject bullet;
+    public GameObject bullet_1;
+    public GameObject bullet_2;
+    private GameObject selectedBullet;
     public Transform startPos;
-    public int maxShotsPerGame = 3;
+    public int maxShotsPerGameBullet1 = 3;
+    public int maxShotsPerGameBullet2 = 3;
 
-    private int _shotsLeft;
+    private int _shotsLeft1;
+    private int _shotsLeft2;
 
     void Start()
     {
-        _shotsLeft = maxShotsPerGame;
+        _shotsLeft1 = maxShotsPerGameBullet1;
+        _shotsLeft2 = maxShotsPerGameBullet2;
+        selectedBullet = bullet_1;
     }
 
     public void Shoot(InputAction.CallbackContext context)
     {
-        if (!context.started || _shotsLeft <= 0)
+        if (!context.started)
             return;
 
-        _shotsLeft--;
-        GameObject bulletClone = Instantiate(bullet, startPos.position, startPos.rotation);
+        if (selectedBullet == bullet_1)
+        {
+            if (_shotsLeft1 <= 0)
+                return;
+            _shotsLeft1--;
+        }
+        else if (selectedBullet == bullet_2)
+        {
+            if (_shotsLeft2 <= 0)
+                return;
+            _shotsLeft2--;
+        }
+        else
+            return;
+        GameObject bulletClone = Instantiate(selectedBullet, startPos.position, startPos.rotation);
         Destroy(bulletClone, 1);
     }
 
     public void RefillAmmo()
     {
-        _shotsLeft = maxShotsPerGame;
+        _shotsLeft1 = maxShotsPerGameBullet1;
+        _shotsLeft2 = maxShotsPerGameBullet2;
+    }
+
+    public void SelectBullet(InputAction.CallbackContext context)
+    {
+        if (!context.started)
+            return;
+
+        var path = context.control.path;
+        if (path == "/Keyboard/1" || path == "<Keyboard>/1")
+            selectedBullet = bullet_1;
+        else if (path == "/Keyboard/2" || path == "<Keyboard>/2")
+            selectedBullet = bullet_2;
     }
 }
---
+++ Plataforma/Assets/_Scripts/Weapons/Bullet_2.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_2 : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        _rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+        else if(collision.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
